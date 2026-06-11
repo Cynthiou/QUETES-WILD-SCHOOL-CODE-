548aafacf17719f4dcadc1dc25505b391a468787
Cynthiou <cynthia.flores@hotmail.fr>
2026-06-11 15:56:51 +0200

    React Challenge 6

--- Semaine_5/pokedex/src/App.tsx
+++ Semaine_5/pokedex/src/App.tsx
@@ -1,28 +1,44 @@
 //========== IMPORT =============//
 
+import { useState } from "react";
 import "./App.css";
-import PokemonCard from "./components/PokemonCard"
+
+import PokemonCard from "./components/PokemonCard";
+
 //========== CONTENUE ===========//
 
 const pokemonList = [
   {
-    name: "bulbasaur",
+    name: "Bulbasaur",
     imgSrc:
       "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
   },
   {
-    name: "mew",
+    name: "Mew",
+    imgSrc:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/151.png",
   },
 ];
 
 function App() {
+  const [pokemonName, setPokemonName] = useState("Bulbasaur");
+
+  const pokemon = pokemonList.find((pokemon) => pokemon.name === pokemonName);
+
+  if (pokemon == null) {
+    throw new Error("Invalid pokemon name");
+  }
+
   return (
-  <div>
-    <PokemonCard pokemon={pokemonList[0]}/>
-  </div>
+    <div>
+      <PokemonCard pokemon={pokemon} />
+      <div>
+        <button onClick={() => setPokemonName("Bulbasaur")}>Bulbasaur</button>
+        <button onClick={() => setPokemonName("Mew")}>Mew</button>
+      </div>
+    </div>
   );
 }
-
 //========== EXPORT =============//
 
 export default App;
